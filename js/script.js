9d067e7a669785f77f5054ecfceed72be9fa0d3d
rafaellabastos1 <rm552425@fiap.com.br>
2023-05-18 08:44:33 -0300

    criei classes para os campos do formulário e para os botões, retirei alguns <br> e troquei o nome dos métodos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Confirmar escolha do tipo de seguro
-function mensagem(tipoSeguro) {
-    let mensagemElement = document.getElementById("mensagem");
+function escolha(tipoSeguro) {
+    let mensagemElement = document.getElementById("escolha");
     if (tipoSeguro == '1'){
         mensagemElement.innerHTML = "▶ Seguro selecionado para ciclistas que pedalam na rua";
     } else if (tipoSeguro == '2') {
@@ -18,12 +18,12 @@ function mensagem(tipoSeguro) {
     }
 }
 
-function mensagem(statusVistoria) {
-    let mensagemElement = document.getElementById("mensagem");
+function status(statusVistoria) {
+    let mensagemElement = document.getElementById("status");
     mensagemElement.innerHTML = "▶ Aqui será mostrado o atual status da vistoria do cliente com base nas análises da IA.";
 }
 
-function mensagem(apolice) {
-    let mensagemElement = document.getElementById("mensagem");
+function emissao(apolice) {
+    let mensagemElement = document.getElementById("emissao");
     mensagemElement.innerHTML = "▶ Sua apólice foi enviada para o e-mail cadastrado no processo de vistoria. Acesse e confira. Nela constará seus dados pessoais, os dados de sua bike, o tipo de seguro escolhido por você e que você foi aprovado no processo de vistoria. Caso concorde com o que está sendo dito, assine sua apólice e envie de volta pelo mesmo e-mail que enviou a apólice para você";
 }
